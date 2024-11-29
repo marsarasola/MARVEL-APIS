1c08a6544b86d6c8394388990a2c6f8023c14bee
marsarasola <sarasolamartina21@gmail.com>
2024-11-29 12:13:40 -0300

    script

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-const CHARACTERS = []
+const CHARACTERS = [];
 
-const url = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d80711ba6c0f0213843cd530b7f62f73&hash=ecf6a28795aafa3a652fea7ef5b667d8'
-const key = '?ts=1&apikey=d80711ba6c0f0213843cd530b7f62f73&hash=ecf6a28795aafa3a652fea7ef5b667d8'
+const url = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d80711ba6c0f0213843cd530b7f62f73&hash=ecf6a28795aafa3a652fea7ef5b667d8';
+const key = '?ts=1&apikey=d80711ba6c0f0213843cd530b7f62f73&hash=ecf6a28795aafa3a652fea7ef5b667d8';
 
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-const fetchData  = () => {
-
+const fetchData = () => {
    return fetch(url)
-        .then((res)=> res.json())
-        .then((json) =>{ 
+        .then((res) => res.json())
+        .then((json) => {
             CHARACTERS.push(...json.data.results);
-            document.querySelector('button').style.display = 'none'
+            document.querySelector('button').style.display = 'none';
             console.log(CHARACTERS);
         })
-        .catch((err)=>{console.error('HUBO UN ERROR : ' + err)})
-        .finally(console.log('Fetching terminado'))
-}
+        .catch((err) => {
+            console.error('HUBO UN ERROR : ' + err);
+        })
+        .finally(() => console.log('Fetching terminado'));
+};
 
-document.querySelector('button').onclick = async() => {
+document.querySelector('button').onclick = async () => {
     await fetchData();
-    createCards();}
+    createCards();
+};
 
 const createCards = () => {
-    console.log('cards')
-    CHARACTERS.forEach((personaje)=>{
-        createCharacterCard(personaje)
-    })
-}
+    console.log('cards');
+    CHARACTERS.forEach((personaje) => {
+        createCharacterCard(personaje);
+    });
+};
 
 const createCharacterCard = (char) => {
-    const card = document.createElement('div')
-    card.classList.add('card')
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-    const name = document.createElement('h1')
-    name.textContent = char.name
+    const name = document.createElement('h1');
+    name.textContent = char.name;
 
-    const img = document.createElement('img')
-    img.setAttribute('src', char.thumbnail.path+'.'+char.thumbnail.extension) 
+    const img = document.createElement('img');
+    img.setAttribute('src', char.thumbnail.path + '.' + char.thumbnail.extension);
 
-    const link = document.createElement('a')
-    link.setAttribute('href', char.resourceURI+key)
+    const link = document.createElement('a');
+    link.setAttribute('href', char.resourceURI + key);
 
-    const parag = document.createElement('p')
-    parag.textContent = char.description
+    const parag = document.createElement('p');
+    parag.textContent = char.description;
 
-    link.appendChild(img)
-    card.appendChild(name)
-    card.appendChild(link)
-    card.appendChild(parag)
-    body.appendChild(card)
-
-}
+    link.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(link);
+    card.appendChild(parag);
+    body.appendChild(card);
+};
